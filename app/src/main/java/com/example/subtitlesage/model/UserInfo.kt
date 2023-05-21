9613384c7cd446953bdad385c0cdc83103dbea79
guifx <guifxb@yahoo.com>
2023-05-21 18:55:56 -0300

    Added user localization support and Firebase sdk.

--- app/src/main/java/com/example/subtitlesage/model/UserInfo.kt
+++ app/src/main/java/com/example/subtitlesage/model/UserInfo.kt
@@ -25,7 +25,7 @@ data class Experience(
 )
 
 val defaultExp = Experience("", "", "", "")
-val defaultUser = UserInfo(1, "Full name", "professional title", "short summary", "0", "linkedin.com", "example@example.com")
+val defaultUser = UserInfo(1, "", "", "", "", "", "")
 
 
 
